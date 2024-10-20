cb10dd27ab4b3acb8fa8579a9bd4d5209703cb4a
Jimil Soni <jimilsoni926@gmail.com>
2024-10-20 19:02:36 +0530

    added the functionality for adding and removing rooms

--- ClientSide/src/main.jsx
+++ ClientSide/src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import ModernRoomAdminPage from './Components/AddRooms.jsx'
+import DeleteRoomForm from './Components/DeleteRooms.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <BrowserRouter>
-    <App />
+  <App/>
+    {/* <ModernRoomAdminPage/> */}
+    {/* <DeleteRoomForm/> */}
   </BrowserRouter>
   </StrictMode>
   
